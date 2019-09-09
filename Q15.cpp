51b7945b2c74ed09cfbdf0fffc219b681f4045ac
Victornunesp <51335384+Victornunesp@users.noreply.github.com>
2019-09-09 20:05:03 -0300

    Add files via upload

---
+++ Q15.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main (int argc, char *argv[]){
+
+    int rev = argc - 1;
+
+    while(rev > 0) cout << argv[rev--] << " ";
+
+    cout << endl;
+
+    return 0;
+}
